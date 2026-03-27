4e44f690f9fdd39510a450d60a7cea36dcde9495
thijs <thijsessen2@gmial.com>
2026-03-27 02:04:01 +0100

    ffd

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
         index: resolve(__dirname, 'index.html'), // ⭐ ROOT
         login: resolve(__dirname, 'src/pages/login.html'),
         rolenselect: resolve(__dirname, 'src/pages/rolenselect.html'),
+        meldkamer: resolve(__dirname, 'src/pages/panels/meldkamer.html'),
+        burger: resolve(__dirname, 'src/pages/panels/burger.html'),
+        admin: resolve(__dirname, 'src/pages/panels/admin.html'),
         wachtwoordvergeten: resolve(__dirname, 'src/pages/wachtwoordvergeten.html'),
       }
     }
