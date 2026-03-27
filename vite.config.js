6820376167f78ef30bb5643bc071365d03bc28d1
thijs <thijsessen2@gmial.com>
2026-03-27 01:31:22 +0100

    443d

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: {
+        index: resolve(__dirname, '/index.html'),
         login: resolve(__dirname, 'src/pages/login.html'),
         wachtwoordvergeten: resolve(__dirname, 'src/pages/wachtwoordvergeten.html'),
         error: resolve(__dirname, 'src/pages/error.html'),
