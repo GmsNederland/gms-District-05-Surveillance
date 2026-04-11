5e6bc0d987e3fb8ba64afa9879d90b4592806ef9
thijs <thijsessen2@gmial.com>
2026-04-11 15:02:43 +0200

    ffd

--- src/codes/meldkamer.js
+++ src/codes/meldkamer.js
@@ -1,12 +1,12 @@
  // Firebase config (vul in met jouw eigen project config)
   const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://PROJECT_ID.firebaseio.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID"
+      apiKey: "AIzaSyBRZCtv2Gr145LSlZw1QHIIkKXSHZoUk-U",
+      authDomain: "district05surveillance-e5183.firebaseapp.com",
+      databaseURL: "https://district05surveillance-e5183-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "district05surveillance-e5183",
+      storageBucket: "district05surveillance-e5183.firebasestorage.app",
+      messagingSenderId: "492638820842",
+      appId: "1:492638820842:web:a3ff331099f9f4e88efa93"
   };
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
